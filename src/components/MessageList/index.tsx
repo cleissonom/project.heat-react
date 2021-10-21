7ed6911716bbbff5a52e212a29d3f68a5c0ed5e8
Cleisson <cleissonconstc@gmail.com>
2021-10-21 14:52:24 -0300

    creating Form to send messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -6,6 +6,7 @@ import styles from "./styles.module.scss"
 import logoImg from '../../assets/logo.svg'
 
 interface Message {
+   created_at: string,
    id: string,
    text: string,
    user: {
@@ -30,13 +31,16 @@ export const MessageList = () => {
          <ul className={styles.messageList}>
             {messages.map((message) => {
                return (
-                  <li className={styles.message}>
+                  <li key={message.id} className={styles.message}>
                      <p className={styles.messageContent}>{message.text}</p>
                      <div className={styles.messageUser}>
                         <div className={styles.userImage}>
                            <img src={message.user.avatar_url} alt={message.user.name} />
                         </div>
                         <span>{message.user.name}</span>
+                     </div>
+                     <div className={styles.messageDate}>
+                        {new Date(message.created_at).toLocaleDateString()}
                      </div>
                   </li>
                )
