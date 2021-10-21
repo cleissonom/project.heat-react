74486136ee2cb0540897e9c6318f3e0dd13880f4
Cleisson <cleissonconstc@gmail.com>
2021-10-21 11:03:29 -0300

    first commit

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles.module.scss"
+
+import logoImg from '../../assets/logo.svg'
+
+export const MessageList = () => {
+   return (
+      <div className={styles.messageListWrapper}>
+         <img src={logoImg} alt="Logo DoWhile 2021" />
+
+         <ul className={styles.messageList}>
+            <li className={styles.message}>
+               <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+               <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                     <img src="https://github.com/cleissonom.png" alt="Cleisson Oliveira" />
+                  </div>
+                  <span>Cleisson Oliveira</span>
+               </div>
+            </li>
+            <li className={styles.message}>
+               <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+               <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                     <img src="https://github.com/cleissonom.png" alt="Cleisson Oliveira" />
+                  </div>
+                  <span>Cleisson Oliveira</span>
+               </div>
+            </li>
+            <li className={styles.message}>
+               <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+               <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                     <img src="https://github.com/cleissonom.png" alt="Cleisson Oliveira" />
+                  </div>
+                  <span>Cleisson Oliveira</span>
+               </div>
+            </li>
+         </ul>
+      </div>
+   )
+}
